4cdbac814cb0caf82c6f28b2c78fc0975a86b849
Jagna Juras <j.juras.403@studms.ug.edu>
2024-12-01 16:45:50 +0100

    Dodano podstawowe wyświetlanie listy roślin na stronie głównej

--- frontend/app/src/main/java/com/example/planttrackerapp/ui/PlantList.kt
+++ frontend/app/src/main/java/com/example/planttrackerapp/ui/PlantList.kt
@@ -2,37 +2,51 @@ package com.example.planttrackerapp.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.planttrackerapp.data.Datasource
+import com.example.planttrackerapp.model.Plant
+import com.example.planttrackerapp.ui.components.SinglePlantCard
 
 @Composable
 fun PlantList(
-    onClickA: () -> Unit = {},
+    // onClickA: () -> Unit = {},
     onClickB: () -> Unit = {},
+    onClick: (Plant) -> Unit,
     modifier: Modifier = Modifier
 ){
-    Column {
-        Text(
-            text = "PlantApp"
-        )
-        Button(
-            onClick = {
-                onClickA()
-            }
-        ) {
-            Text(
-                text = "A"
-            )
-        }
-        Button(
-            onClick = { onClickB() }
-        ) {
-            Text(
-                text = "B"
-            )
+//    Column {
+//        Text(
+//            text = "PlantApp"
+//        )
+//        Button(
+//            onClick = {
+//                onClickA()
+//            }
+//        ) {
+//            Text(
+//                text = "A"
+//            )
+//        }
+//        Button(
+//            onClick = { onClickB() }
+//        ) {
+//            Text(
+//                text = "B"
+//            )
+//        }
+//    }
+// Trzeba dodać do roślin onClick żeby dało się przechodzić do ich detali (SinglePlantView)
+    val plants = Datasource.plantList
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(plants.size) { index ->
+            SinglePlantCard(
+                plant = plants[index],
+                onItemClick = onClick)
         }
     }
-
 }

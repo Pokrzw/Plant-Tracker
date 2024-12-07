498232f5540b680eb7f7b82a8eadde1d5b7c7023
Jagna Juras <j.juras.403@studms.ug.edu>
2024-12-07 18:03:13 +0100

    Pliki z Material Theme Builder

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.generativeai)
     implementation(libs.firebase.vertexai)
+    implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

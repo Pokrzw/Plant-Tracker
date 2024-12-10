0d80ddf77a8012fa5655cebd689bc08acadef7a0
Jagna Juras <j.juras.403@studms.ug.edu>
2024-12-10 17:18:26 +0100

    Uwzględiono zmiany z modelu

--- frontend/app/src/main/java/com/example/planttrackerapp/data/FormUiState.kt
+++ frontend/app/src/main/java/com/example/planttrackerapp/data/FormUiState.kt
@@ -8,7 +8,7 @@ data class FormUiState(
     val id: Int = 0,
     val name: String = "",
     val species: Species? = null,
-    val lastWatered: Calendar? = null,
+    val waterHistory: List<Calendar>? = emptyList(),
     val created: Calendar? = null,
     val speciesList: List<Species> = emptyList(),
     val plantsList: List<Plant> = emptyList()
